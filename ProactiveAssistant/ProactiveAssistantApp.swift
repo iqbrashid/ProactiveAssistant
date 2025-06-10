e13855deda39c380a603bfcedfa262c679328e52
iqbrashid <iqbrashid@gmail.com>
2025-06-10 14:52:38 -0700

    Initial Commit

---
+++ ProactiveAssistant/ProactiveAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProactiveAssistantApp.swift
+//  ProactiveAssistant
+//
+//  Created by Rashid Iqbal on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ProactiveAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProactiveAssistantTests/ProactiveAssistantTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProactiveAssistantTests.swift
+//  ProactiveAssistantTests
+//
+//  Created by Rashid Iqbal on 6/10/25.
+//
+
+import Testing
+
+struct ProactiveAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
